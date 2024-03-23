e2ef6de6761e3263e89f21707c2a124c1cc441ca
Chengyenn <a0987206119@gmail.com>
2024-03-23 14:50:27 +0800

    add images, help document and commonLayout

---
+++ src/app/commonLayout/page.js
@@ -0,0 +1,173 @@
+// import Image from "next/image";
+import Link from "next/link";
+
+const categories = [
+  {
+    name: "FISH",
+    productLink: "",
+    image: "/images/sm_fish.gif",
+  },
+  {
+    name: "DOGS",
+    productLink: "",
+    image: "/images/sm_dogs.gif",
+  },
+  {
+    name: "REPTILES",
+    productLink: "",
+    image: "/images/sm_reptiles.gif",
+  },
+  {
+    name: "CATS",
+    productLink: "",
+    image: "/images/sm_cats.gif",
+  },
+  {
+    name: "BIRDS",
+    productLink: "",
+    image: "/images/sm_birds.gif",
+  },
+];
+
+export default function CommonLayout() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+// Header
+function Header() {
+  return (
+    <div id="Header">
+      <Logo />
+      <Menu />
+      <Search />
+      <QuickLink />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div id="Logo">
+      <div id="LogoContent">
+        <a href="">
+          <img src="/images/logo-topbar.gif" alt="logo" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <div id="Menu">
+      <div id="MenuContent">
+        <a href="">
+          <img src="/images/cart.gif" alt="img_car" align="middle" />
+        </a>
+        <img src="/images/separator.gif" align="middle" />
+        <a href="">Sign In</a>
+        {/* <a href="">Sign Out</a> */}
+        {/* <img src="/images/separator.gif" align="middle" />
+        <a href="">My Account</a> */}
+        <img src="/images/separator.gif" align="middle" />
+        <a href="/help" target="help">
+          ?
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div id="Search">
+      <div id="SearchContent">
+        <form>
+          <input name="keywords" size="14" />
+          <button id="searchProducts">Search</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+function QuickLink() {
+  return (
+    <div id="QuickLinks">
+      {categories.map((product) => (
+        <QuickLinkList categotyObj={product} key={product.name}>
+          <img src="/images/separator.gif" />
+        </QuickLinkList>
+      ))}
+    </div>
+  );
+}
+
+function QuickLinkList({ categotyObj, children }) {
+  return (
+    <>
+      <a href={categotyObj.productLink}>
+        <img src={categotyObj.image} />
+      </a>
+      {children}
+    </>
+  );
+}
+
+// Content
+function Content() {
+  return (
+    <div id="Content">
+      {/* <Message /> */}
+      <Section />
+    </div>
+  );
+}
+
+function Message() {
+  <ul class="messages">
+    <li
+    // th:each="message : ${messages}"
+    // th:class="${message.type}"
+    // th:text="${message.text}"
+    ></li>
+  </ul>;
+}
+
+function Section({ children }) {
+  return <section>{children}</section>;
+}
+
+// Footer
+function Footer() {
+  return (
+    <div id="Footer">
+      <PowerBy />
+      <Banner />
+    </div>
+  );
+}
+
+function PowerBy() {
+  return (
+    <div id="PoweredBy">
+      <a href="http://kazuki43zoo.com" target="homepage">
+        kazuki43zoo.com
+      </a>
+    </div>
+  );
+}
+
+function Banner() {
+  return (
+    <div id="Banner">
+      <span>Banner</span>
+    </div>
+  );
+}
---
+++ src/app/help/page.js
@@ -0,0 +1,17 @@
+export default function Help() {
+  return (
+    <div id="Content">
+      <h1>JPetStore Demo</h1>
+      <p>
+        The JPetStore Demo is an online pet store. Like most e-stores, you can
+        browse and search the product catalog, choose items to add to a shopping
+        cart, amend the shopping cart, and order the items in the shopping cart.
+        You can perform many of these actions without registering with or
+        logging into the application. However, before you can order items you
+        must log in (sign in) to the application. In order to sign in, you must
+        have an account with the application, which is created when you register
+        (sign up) with the application.
+      </p>
+    </div>
+  );
+}
