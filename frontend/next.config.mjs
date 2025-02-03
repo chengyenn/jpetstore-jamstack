2b97618ebe8824e9ffa0c4da756de5fd311c7a92
chengyenn <a0987206119@gmail.com>
2025-02-03 14:28:11 +0800

    Remove headers to fix Vercel jwe issue

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/order/orderInfo/:id",
-        headers: [
-          { key: "Cache-Control", value: "public, max-age=0, must-revalidate" },
-        ],
-      },
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/order/orderInfo/:id",
+  //       headers: [
+  //         { key: "Cache-Control", value: "public, max-age=0, must-revalidate" },
+  //       ],
+  //     },
+  //     {
+  //       source: "/:path*",
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value: "public, max-age=31536000, immutable",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
